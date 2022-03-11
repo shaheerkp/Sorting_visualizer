574b02cd80ead12f21527622636cb829a39f10d4
shaheerkp <kpshaheer123@gmail.com>
2022-03-11 13:51:21 +0530

    sort problem fixed

--- src/App.js
+++ src/App.js
@@ -2,46 +2,35 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([2,1,4,3]);
+  const [data, setData] = useState([5, 2, 1, 8, 16, 10, 4, 3]);
 
   useEffect(() => {
-    
     console.log("eeee");
-   
-    
-    
   }, []);
-  
-  
 
-  const a = () => {
-    
-    for (let i = 0; i < data.length - 1; i++) {
-     
-      for (let j = i + 1; j < data.length; j++) {
-     
-        if (data[i] > data[j]) {
-          let temp = data[j];
-          let tempArr=[...data]
-          console.log("t",tempArr);
-          console.log("d",data);
-          console.log("$$$$$$$$$$$$$$$$$$$$$$");
-          tempArr[j]=tempArr[i]
-          tempArr[i]=temp;
-          setData(tempArr)
-          console.log("t",tempArr);
-          console.log("d",data);
-          
+  const sort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] > arr[j]) {
+          let temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
         }
-       
       }
     }
+    return arr
   };
- 
+
+  const a = () => {
+    let tempArr = sort(data)
+    console.log(data);
+     setData([...tempArr]);
+     console.log(data);
+  };
 
   return (
     <>
-      <button onClick={a}  >start</button>
+      <button onClick={a}>start</button>
       <div
         style={{
           padding: "5px",
