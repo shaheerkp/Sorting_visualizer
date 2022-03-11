75e7b610408f7b6558605603668a1df1b59a0dad
shaheerkp <kpshaheer123@gmail.com>
2022-03-11 12:25:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([2,1,4,3]);
+
+  useEffect(() => {
+    
+    console.log("eeee");
+   
+    
+    
+  }, []);
+  
+  
+
+  const a = () => {
+    
+    for (let i = 0; i < data.length - 1; i++) {
+     
+      for (let j = i + 1; j < data.length; j++) {
+     
+        if (data[i] > data[j]) {
+          let temp = data[j];
+          let tempArr=[...data]
+          console.log("t",tempArr);
+          console.log("d",data);
+          console.log("$$$$$$$$$$$$$$$$$$$$$$");
+          tempArr[j]=tempArr[i]
+          tempArr[i]=temp;
+          setData(tempArr)
+          console.log("t",tempArr);
+          console.log("d",data);
+          
+        }
+       
+      }
+    }
+  };
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={a}  >start</button>
+      <div
+        style={{
+          padding: "5px",
+          background: "black",
+          alignContent: "center",
+          margin: "auto",
+          width: "500px",
+        }}
+        className="App"
+      >
+        {data.map((ele, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                marginLeft: "5px",
+                background: "red",
+                width: "20px",
+                display: "inline-block",
+                height: `${ele * 10}px`,
+              }}
+            >
+              {ele}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
